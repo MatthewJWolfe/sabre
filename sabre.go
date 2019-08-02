4ddf537ccc76f9a0db1ac84446ecfaa933798ea6
Matthew Wolfe <mjw.wolfe@gmail.com>
2019-08-02 18:46:59 +0000

    initial commit

---
+++ sabre.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("In George Lucas' Star Wars trilogy, Jedi Knights were expected to make their own light sabers. The message was clear: a warrior confronted by a powerful empire bent on totalitarian control must be self-reliant. As we face a real threat of a ban on the distribution of strong cryptography, in the United States and possibly world-wide, we should emulate the Jedi masters by learning how to build strong cryptography programs all by ourselves. If this can be done, strong cryptography will become impossible to suppress.")
+}
